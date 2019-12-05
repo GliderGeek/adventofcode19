73ece211dd1fcb17c66716680d37c15b0910e536
GliderGeek <glidergeek@users.noreply.github.com>
2019-12-05 23:48:03 +0100

    cleaned up

--- day5/solution.go
+++ day5/solution.go
@@ -43,27 +43,19 @@ func parse_instruction(instruction int) (int, []int) {
     var parameter_modes []int
     remainder := instruction
 
-    if opcode == 3 || opcode == 4 { //only single parameter_mode
-        parameter_modes = append(parameter_modes, instruction / 100)
-    } else if opcode == 5 || opcode == 6{ //two parameter_modes
-        second_mode := remainder / 1000
-        remainder -= second_mode * 1000
-        first_mode := remainder / 100
+    third_mode := instruction / 10000
+    remainder -= third_mode * 10000
+    second_mode := remainder / 1000
+    remainder -= second_mode * 1000
+    first_mode := remainder / 100
 
-        // this can probably be a lot cleaner
-        parameter_modes = append(parameter_modes, first_mode)
-        parameter_modes = append(parameter_modes, second_mode)
-    } else if opcode == 1 || opcode == 2 || opcode == 7 || opcode == 8 { //three parameter modes
-        third_mode := instruction / 10000
-        remainder -= third_mode * 10000
-        second_mode := remainder / 1000
-        remainder -= second_mode * 1000
-        first_mode := remainder / 100
-
-        // this can probably be a lot cleaner
-        parameter_modes = append(parameter_modes, first_mode)
-        parameter_modes = append(parameter_modes, second_mode)
-        parameter_modes = append(parameter_modes, third_mode)
+    switch opcode{
+    case 3, 4: //only single parameter_mode
+        parameter_modes = []int{first_mode}
+    case 5, 6: //two parameter_modes
+        parameter_modes = []int{first_mode, second_mode}
+    case 1, 2, 7, 8: //three parameter modes
+        parameter_modes = []int{first_mode, second_mode, third_mode}
     }
 
     return opcode, parameter_modes
@@ -82,41 +74,29 @@ func get_value(values []int, parameter_mode int, parameter_index int) int {
 
 func get_last_output(filename string, input int) int{
 
-    fmt.Println("input", input)
-
+    var value1, value2, output int
     values := get_input(filename)
     instruction_index := 0
     instruction := values[instruction_index]
     opcode, parameter_modes := parse_instruction(instruction)
-    var value1, value2, output int
 
     for opcode != 99 {   
 
-        // fmt.Println("values", values)
-        // fmt.Println("instruction_index", instruction_index)
-        // fmt.Println("instruction", instruction)
-        // fmt.Println("opcode", opcode)
-        // fmt.Println("parameter_modes", parameter_modes)
-
-
-        if opcode == 1 || opcode == 2{
-
+        switch opcode {
+        case 1:
             value1 = get_value(values, parameter_modes[0], instruction_index+1)
             value2 = get_value(values, parameter_modes[1], instruction_index+2)
-
-            if opcode == 1 {
-                values[values[instruction_index+3]] = value1 + value2
-            } else { //opcode == 2
-                values[values[instruction_index+3]] = value1 * value2
-            }
-
+            values[values[instruction_index+3]] = value1 + value2
             instruction_index += 4
-            
-        } else if opcode == 3 {
+        case 2:
+            value1 = get_value(values, parameter_modes[0], instruction_index+1)
+            value2 = get_value(values, parameter_modes[1], instruction_index+2)
+            values[values[instruction_index+3]] = value1 * value2
+            instruction_index += 4
+        case 3:
             values[values[instruction_index+1]] = input
             instruction_index += 2
-
-        } else if opcode == 4 {
+        case 4:
             if parameter_modes[0] == 0{
                 output = values[values[instruction_index+1]]
             } else {
@@ -124,8 +104,7 @@ func get_last_output(filename string, input int) int{
             }
             fmt.Println("outputting", output)
             instruction_index += 2
-        } else if opcode == 5 {
-
+        case 5:
             value1 = get_value(values, parameter_modes[0], instruction_index+1)
             value2 = get_value(values, parameter_modes[1], instruction_index+2)
 
@@ -135,8 +114,7 @@ func get_last_output(filename string, input int) int{
             default:
                 instruction_index = value2
             }
-
-        } else if opcode == 6 {
+        case 6:
             value1 = get_value(values, parameter_modes[0], instruction_index+1)
             value2 = get_value(values, parameter_modes[1], instruction_index+2)
 
@@ -146,7 +124,7 @@ func get_last_output(filename string, input int) int{
             default:
                 instruction_index += 3
             }
-        } else if opcode == 7 {
+        case 7:
             value1 = get_value(values, parameter_modes[0], instruction_index+1)
             value2 = get_value(values, parameter_modes[1], instruction_index+2)
 
@@ -155,10 +133,8 @@ func get_last_output(filename string, input int) int{
             } else {
                 values[values[instruction_index+3]] = 0
             }
-
             instruction_index += 4
-
-        } else if opcode == 8 {
+        case 8:
             value1 = get_value(values, parameter_modes[0], instruction_index+1)
             value2 = get_value(values, parameter_modes[1], instruction_index+2)
 
@@ -169,12 +145,10 @@ func get_last_output(filename string, input int) int{
             }
 
             instruction_index += 4
-
-        } else {
+        default:
             fmt.Println(opcode)
             panic("Unexpected fault")
         }
-        
         instruction = values[instruction_index]
         opcode, parameter_modes = parse_instruction(instruction)
     } 
