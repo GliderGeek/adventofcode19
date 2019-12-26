1dfd28be0314233b4013ca5e05b40c9695bb4068
Matthijs <mbeekman@viktor.ai>
2019-12-26 19:40:50 +0100

    uncleaned solution part 2

--- day6/solution.go
+++ day6/solution.go
@@ -11,6 +11,7 @@ import (
 type node struct {
 	name     string
 	children []*node
+	parents  []*node
 }
 
 func getInput(fileName string) [][2]string {
@@ -47,24 +48,25 @@ func getInput(fileName string) [][2]string {
 
 func getOrbitMap(entries [][2]string) map[string]*node {
 	nodes := map[string]*node{
-		"COM": &node{"COM", []*node{}},
+		"COM": &node{"COM", []*node{}, []*node{}},
 	}
 
 	for _, entry := range entries {
 		parentNode, present := nodes[entry[0]]
 		if !present {
-			parentNode = &node{entry[0], []*node{}}
+			parentNode = &node{entry[0], []*node{}, []*node{}}
 			nodes[parentNode.name] = parentNode
 		}
 
 		childNode, present := nodes[entry[1]]
 		if !present {
-			childNode = &node{entry[1], []*node{}}
+			childNode = &node{entry[1], []*node{}, []*node{}}
 			nodes[childNode.name] = childNode
 
 		}
 
 		parentNode.children = append(parentNode.children, childNode)
+		childNode.parents = append(childNode.parents, parentNode)
 	}
 
 	return nodes
@@ -84,6 +86,33 @@ func main() {
 	input := getInput("input.txt")
 	orbitMap := getOrbitMap(input)
 	fmt.Println("orbits", calculateOrbits(orbitMap["COM"].children, 1))
+
+	total := 0
+
+	visited := map[string]int{}
+	node := orbitMap["YOU"]
+	distance := 0
+	for len(node.parents) != 0 {
+		node = node.parents[0]
+		visited[node.name] = distance
+		distance++
+	}
+
+	node = orbitMap["SAN"]
+	distance = 0
+	for len(node.parents) != 0 {
+		node = node.parents[0]
+		count, present := visited[node.name]
+		if present {
+			total = count + distance
+			break
+		} else {
+			visited[node.name] = distance
+		}
+		distance++
+	}
+
+	fmt.Println(total)
 
 	// fmt.Println("Result part 1 is:", get_last_output("input.txt", 1))
 	// fmt.Println("Result part 2 is:", get_last_output("input.txt", 5))
